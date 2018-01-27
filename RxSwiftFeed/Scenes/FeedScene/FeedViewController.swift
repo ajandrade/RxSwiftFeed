1707cf64f13b8c17427a8b65dcc129293b73929f
Amadeu Andrade <amadeu.dev@gmail.com>
2018-01-27 18:39:53 +0000

    Updated with initial UI

--- RxSwiftFeed/Scenes/FeedScene/FeedViewController.swift
+++ RxSwiftFeed/Scenes/FeedScene/FeedViewController.swift
@@ -14,11 +14,31 @@ class FeedViewController: UIViewController {
   
   var viewModel: FeedViewModelRepresentable!
   
+  // MARK: - IBOUTLETS
+  
+  @IBOutlet private weak var tableView: UITableView! {
+    didSet { tableView.refreshControl = UIRefreshControl() }
+  }
+  
   // MARK: - VIEW LIFE CYCLE
   
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "RxSwiftFeed"
+    setRefreshControl()
+  }
+  
+  private func setRefreshControl() {
+    let refreshControl = tableView.refreshControl
+    refreshControl?.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+    refreshControl?.tintColor = UIColor.darkGray
+    refreshControl?.attributedTitle = NSAttributedString(string: "Pull to refresh")
+    refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+  }
+  
+  @objc func refresh() {
+    print("Refresh")
+    tableView.refreshControl?.endRefreshing()
   }
   
 }
