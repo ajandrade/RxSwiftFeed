b6f55a3f738488a3f988d21c68d39fcc62c4aa9c
Amadeu Andrade <amadeu.dev@gmail.com>
2018-01-31 21:55:12 +0000

    Updated feed view model tests

--- RxSwiftFeedTests/Cases/Scenes/FeedViewModelTests.swift
+++ RxSwiftFeedTests/Cases/Scenes/FeedViewModelTests.swift
@@ -15,12 +15,9 @@ class FeedViewModelTests: XCTestCase {
   
   var viewModel: FeedViewModel!
   var bag: DisposeBag!
-
+  
   override func setUp() {
     super.setUp()
-    let githubProvider = MockGithubAPIClient()
-    let dependencyContainer = DependencyContainer(githubProvider: githubProvider)
-    viewModel = FeedViewModel(dependencies: dependencyContainer)
     bag = DisposeBag()
   }
   
@@ -31,21 +28,64 @@ class FeedViewModelTests: XCTestCase {
   }
   
   func testFetchEvents() {
+    let githubProvider = MockGithubAPIClient(type: .success)
+    let dependencyContainer = DependencyContainer(githubProvider: githubProvider)
+    viewModel = FeedViewModel(dependencies: dependencyContainer)
+    
     let someExpectation = expectation(description: "Fetch events succesfully")
-
+    
     viewModel.fetch()
     
     viewModel.eventViewModels
       .drive(onNext: { eventViewModels in
         XCTAssertGreaterThan(eventViewModels.count, 0)
         someExpectation.fulfill()
-    })
-    .disposed(by: bag)
+      })
+      .disposed(by: bag)
     
     wait(for: [someExpectation], timeout: 1)
   }
   
-  // TODO: - Added test for empty result
-  // TODO: - Added test with error while fetching
+  func testFetchEventsWithEmptyResponse() {
+    let githubProvider = MockGithubAPIClient(type: .empty)
+    let dependencyContainer = DependencyContainer(githubProvider: githubProvider)
+    viewModel = FeedViewModel(dependencies: dependencyContainer)
+    
+    let someExpectation = expectation(description: "Parse failed!")
+    
+    viewModel.fetch()
+    
+    viewModel.eventViewModels
+      .drive(onNext: { eventViewModels in
+        XCTAssertEqual(eventViewModels.count, 0)
+        someExpectation.fulfill()
+      })
+      .disposed(by: bag)
+    
+    wait(for: [someExpectation], timeout: 1)
+  }
+  
+  func testFetchEventsWithError() {
+    let githubProvider = MockGithubAPIClient(type: .failure)
+    let dependencyContainer = DependencyContainer(githubProvider: githubProvider)
+    viewModel = FeedViewModel(dependencies: dependencyContainer)
+    
+    let errorExpectation = expectation(description: "Error")
+    
+    viewModel.fetch()
+    
+    viewModel.eventViewModels
+      .asObservable()
+      .subscribe(onNext: { events in
+        if events.isEmpty {
+          errorExpectation.fulfill()
+        } else {
+          XCTFail()
+        }
+      })
+      .disposed(by: bag)
+    
+    wait(for: [errorExpectation], timeout: 1)
+  }
   
 }
--- RxSwiftFeedTests/Mocks/MockGithubAPIClient.swift
+++ RxSwiftFeedTests/Mocks/MockGithubAPIClient.swift
@@ -14,12 +14,29 @@ import RxCocoa
 
 class MockGithubAPIClient: GithubAPIClientProtocol {
   
+  private let type: DataType
+  
+  enum DataType { case success, failure, empty }
+  
+  init(type: DataType) {
+    self.type = type
+  }
+  
   func fetchEvents(for identifier: String) -> Single<Data> {
     let repository = Repository(name: "repo")
     let actor = Actor(name: "actor", avatarUrlString: "")
     let event = Event(repository: repository, actor: actor, action: "action")
     let jsonEncoder = JSONEncoder()
-    let jsonData = try! jsonEncoder.encode([event])
+    let jsonData: Data
+    switch type {
+    case .success:
+      jsonData = try! jsonEncoder.encode([event])
+    case .empty:
+      jsonData = try! jsonEncoder.encode(event)
+    case .failure:
+      let err = NetworkError.unknown
+      return Single.error(err)
+    }
     return Single.just(jsonData)
   }
   
